6f427804c168e99b52e80ce2c6acb0afae84f48f
VishmiRamona <vishmiramona@gmail.com>
2026-07-08 20:20:32 +0530

    Fix 13

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://miniinventory-backend.azurewebsites.net/api',
+  baseURL: 'https://miniinventory-backend.azurewebsites.net/api/',
   headers: {
     'Content-Type': 'application/json',
   },
